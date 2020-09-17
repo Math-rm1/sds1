9bf7ce8d49b0df79f62e51b31e846ff97aeafe01
Matheus Rodrigues Mariano <matheus.r.mariano@hotmail.com>
2020-09-17 18:22:12 -0300

    Entities, Repositories

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
@@ -0,0 +1,117 @@
+package com.devsuperior.dspesquisa.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.devsuperior.dspesquisa.entities.enums.Platform;
+
+@Entity
+@Table(name = "tb_game")
+public class Game implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	// the id attribute will be the primary key of the table game
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String title;
+	private Platform platform;
+
+	@ManyToOne
+	@JoinColumn(name = "genre_id")
+	private Genre genre;
+
+	@OneToMany(mappedBy = "game")
+	private List<Record> records = new ArrayList<>();
+
+	// Constructor Functions
+
+	public Game() {
+
+	}
+
+	public Game(Long id, String title, Platform platform, Genre genre) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.platform = platform;
+		this.genre = genre;
+	}
+
+	// Getter and Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Platform getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(Platform platform) {
+		this.platform = platform;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
+	public List<Record> getRecords() {
+		return records;
+	}
+
+	// hashCode and equals
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
