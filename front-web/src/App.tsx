2da1e05f5103cf69524094bf43424474d4f8c042
Matheus Rodrigues Mariano <matheus.r.mariano@hotmail.com>
2020-09-19 20:51:39 -0300

    Criando pagina Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/home';
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
 
   );
 }
 
 export default App;
+
