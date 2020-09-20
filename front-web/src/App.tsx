15b77080d5a7468ed36fd15d95e37c5d0821d72a
Matheus Rodrigues Mariano <matheus.r.mariano@hotmail.com>
2020-09-20 10:37:17 -0300

    Adicionando a página de gráficos

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Route } from 'react-router-dom';
 import './App.css';
 import Routes from './routes';
 
