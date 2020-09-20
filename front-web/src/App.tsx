8882680fd57807b38fa0f623debd01c3fb6a532c
Matheus Rodrigues Mariano <matheus.r.mariano@hotmail.com>
2020-09-20 07:46:13 -0300

    Criando página de listagem dos registros

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Route } from 'react-router-dom';
 import './App.css';
 import Routes from './routes';
